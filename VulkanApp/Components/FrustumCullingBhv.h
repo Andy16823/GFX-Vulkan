9cc1cb0931cc9e16bafada1fbda8c5b589f54784
Andreas Wagner <andy16823@live.de>
2025-11-13 22:34:26 +0100

    Add detailed documentation to FrustumCullingBhv
    Added XML-style comments to FrustumCullingBhv class members and methods to improve code readability and maintainability. No functional changes were made.

--- VulkanApp/Components/FrustumCullingBhv.h
+++ VulkanApp/Components/FrustumCullingBhv.h
@@ -21,14 +21,39 @@ enum class CullingMode
 class FrustumCullingBhv : public Behavior
 {
 private:
+	/// <summary>
+	/// Pointer to the camera used for culling
+	/// </summary>
 	Camera* m_camera;
+
+	/// <summary>
+	/// The culling mode to use
+	/// </summary>
 	CullingMode m_cullingMode = CullingMode::SPHERE_THEN_AABB_CULLING;
+
+	/// <summary>
+	/// Cached Axis-Aligned Bounding Box of the parent entity
+	/// </summary>
 	AABB m_aabb = AABB();
 
+	/// <summary>
+	/// The half extents of the bounding box
+	/// </summary>
 	glm::vec3 m_halfExtents = glm::vec3(0.0f);
 
+	/// <summary>
+	/// The last known position of the parent transform
+	/// </summary>
 	glm::vec3 m_lastParentPosition = glm::vec3(0.0f);
+
+	/// <summary>
+	/// The last known rotation of the parent transform
+	/// </summary>
 	glm::quat m_lastParentRotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+
+	/// <summary>
+	/// The last known scale of the parent transform
+	/// </summary>
 	glm::vec3 m_lastParentScale = glm::vec3(1.0f);
 
 	/// <summary>
@@ -40,16 +65,48 @@ private:
 	bool isDirty(Transform& transform);
 
 public:
+
+	/// <summary>
+	/// Creates a frustum culling behavior with the specified camera and culling mode
+	/// </summary>
+	/// <param name="camera"></param>
+	/// <param name="mode"></param>
 	FrustumCullingBhv(Camera* camera, CullingMode mode = CullingMode::SPHERE_THEN_AABB_CULLING);
 
+	/// <summary>
+	/// The initialization of the behavior
+	/// </summary>
+	/// <param name="scene"></param>
+	/// <param name="renderer"></param>
 	void init(Scene* scene, Renderer* renderer) override;
 
+	/// <summary>
+	/// The update of the behavior
+	/// </summary>
+	/// <param name="scene"></param>
+	/// <param name="dt"></param>
 	void update(Scene* scene, float dt) override;
 
+	/// <summary>
+	/// Destroys the behavior
+	/// </summary>
+	/// <param name="scene"></param>
+	/// <param name="renderer"></param>
 	void destroy(Scene* scene, Renderer* renderer) override;
 
+	/// <summary>
+	/// Renders the behavior (no-op for culling)
+	/// </summary>
+	/// <param name="scene"></param>
+	/// <param name="renderer"></param>
+	/// <param name="commandbuffer"></param>
+	/// <param name="frame"></param>
 	void render(Scene* scene, Renderer* renderer, VkCommandBuffer commandbuffer, int frame) override;
 
+	/// <summary>
+	/// Gets the unique identifier of this behavior
+	/// </summary>
+	/// <returns></returns>
 	std::string getIdentifier() override;
 };
 
