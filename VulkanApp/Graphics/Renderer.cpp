9dff2ba5555e6d7fddc3548687290b51f0d4d650
Andreas Wagner <andy16823@live.de>
2025-11-28 07:59:23 +0100

    Add fillRect method to Renderer for solid rectangles
    Introduces a fillRect function to the Renderer class, enabling rendering of solid colored rectangles using push constants and descriptor sets. This provides a convenient way to draw filled quads with specified position, size, and color.

--- VulkanApp/Graphics/Renderer.cpp
+++ VulkanApp/Graphics/Renderer.cpp
@@ -2568,6 +2568,30 @@ void Renderer::drawPrimitive(PrimitiveType primitiveType, const glm::mat4& model
 	this->drawBuffers(buffers.vertexBufferIndex, buffers.indexBufferIndex, commandBuffer, 1);
 }
 
+void Renderer::fillRect(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color, VkCommandBuffer commandBuffer, int frame)
+{
+	if (m_activeCamera < 0) {
+		throw std::runtime_error("No camera bound for rectangle rendering!");
+	}
+	auto cameraDescriptorSet = this->getCameraDescriptorSet(m_activeCamera, frame);
+
+	auto primitiveBuffer = m_rendererPrimitives[PrimitiveType::PRIMITIVE_TYPE_QUAD];
+	UboModelColor pushconstant = {};
+	pushconstant.model = glm::translate(glm::mat4(1.0f), glm::vec3(position, 0.0f));
+	pushconstant.model = glm::scale(pushconstant.model, glm::vec3(size, 1.0f));
+	pushconstant.color = color;
+
+	std::array<VkDescriptorSet, 1> descriptorSets = {
+		cameraDescriptorSet
+	};
+
+	this->bindPipeline(commandBuffer, ToString(PipelineType::PIPELINE_TYPE_SOLID_SHADING));
+	auto layout = m_currentPipeline->getPipelineLayout();
+	this->bindPushConstants(commandBuffer, layout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof(UboModelColor), &pushconstant);
+	this->bindDescriptorSets(descriptorSets, 0, frame);
+	this->drawBuffers(primitiveBuffer.vertexBufferIndex, primitiveBuffer.indexBufferIndex, commandBuffer);
+}
+
 /// <summary>
 /// Dispose the renderer and free resources
 /// </summary>
